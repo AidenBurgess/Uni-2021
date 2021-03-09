afcf370c28e0254117d4c5c9cfba32585fc046cd
Aiden Burgess <aiden9874@gmail.com>
2021-03-09 16:16:40 +1300

    Add customers working

--- SOFTENG 750/Labs/2/cs732-lab-02/exercise-01/src/AddCustomerPage.js
+++ SOFTENG 750/Labs/2/cs732-lab-02/exercise-01/src/AddCustomerPage.js
@@ -1,15 +1,89 @@
 import Modal from "./Modal";
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import dayjs from "dayjs";
 
-export default function AddCustomerPage() {
+export default function AddCustomerPage({ addCustomer, customerId }) {
 	const history = useHistory();
+	const [fname, setFName] = useState("");
+	const [lname, setLName] = useState("");
+	const [dob, setDob] = useState("");
+	const [tier, setTier] = useState("Bronze");
+	const [expiryDate, setExpiryDate] = useState("");
+
 	return (
 		<div>
 			<Modal
 				dismissOnClickOutside={true}
 				onCancel={() => history.push("/customers")}
 			>
-				<p>xd</p>
+				<h2>Personal Details</h2>
+				<label for="fname">First name: </label>
+				<input
+					type="text"
+					id="fname"
+					name="fname"
+					value={fname}
+					onInput={(e) => setFName(e.target.value)}
+				/>
+				<br />
+				<br />
+				<label for="lname">Last name: </label>
+				<input
+					type="text"
+					id="lname"
+					name="lname"
+					value={lname}
+					onInput={(e) => setLName(e.target.value)}
+				/>
+				<br />
+				<br />
+				<label for="dob">Date of birth: </label>
+				<input
+					type="date"
+					value={dob}
+					onInput={(e) => setDob(e.target.value)}
+				/>
+				<h2>Membership Details</h2>
+				<label for="tier">Tier: </label>
+				<select
+					name="tier"
+					id="tier"
+					value={tier}
+					onInput={(e) => setTier(e.target.value)}
+				>
+					<option value="Bronze">Bronze</option>
+					<option value="Silver">Silver</option>
+					<option value="Gold">Gold</option>
+					<option value="Platinum">Platinum</option>
+				</select>
+				<br />
+				<br />
+				<label for="expiryDate">Expiry date: </label>
+				<input
+					type="date"
+					value={expiryDate}
+					onInput={(e) => setExpiryDate(e.target.value)}
+				/>
+				<br />
+				<br />
+				<button
+					onClick={() => {
+						addCustomer({
+							id: customerId,
+							firstName: fname,
+							lastName: lname,
+							dob: dayjs(dob),
+							membershipExpires: dayjs(expiryDate),
+							membershipTier: tier,
+						});
+						history.push("/customers");
+					}}
+				>
+					Submit
+				</button>
+				<br />
+				<br />
 			</Modal>
 		</div>
 	);
--- SOFTENG 750/Labs/2/cs732-lab-02/exercise-01/src/CustomersPage.js
+++ SOFTENG 750/Labs/2/cs732-lab-02/exercise-01/src/CustomersPage.js
@@ -5,6 +5,7 @@ import { initialCustomers } from "./data";
 import AddCustomerButton from "./AddCustomerButton";
 import { Switch, Route } from "react-router";
 import AddCustomerPage from "./AddCustomerPage";
+import dayjs from "dayjs";
 
 export default function CustomersPage() {
 	const [customers, setCustomers] = useState(initialCustomers);
@@ -17,7 +18,15 @@ export default function CustomersPage() {
 				</div>
 				<Switch>
 					<Route path="/customers/add">
-						<AddCustomerPage />
+						<AddCustomerPage
+							customerId={customers.length + 1}
+							addCustomer={(newCustomer) => {
+								console.log(newCustomer);
+								console.log(customers[0]);
+								const newState = [...customers, newCustomer];
+								setCustomers(newState);
+							}}
+						/>
 					</Route>
 					<AddCustomerButton />
 				</Switch>
