a94551cb1ef42ea3632fa9b30e035cd8896c4ff0
Aiden Burgess <aiden9874@gmail.com>
2021-04-27 12:10:23 +1200

    Complete task 1

--- SOFTENG 750/Assignments/1/code/frontend/src/App.js
+++ SOFTENG 750/Assignments/1/code/frontend/src/App.js
@@ -1,34 +1,56 @@
-import { Container, Typography, AppBar, Toolbar, makeStyles } from '@material-ui/core';
-import TodoList from './components/TodoList';
+import {
+	Container,
+	Typography,
+	AppBar,
+	Toolbar,
+	makeStyles,
+	CircularProgress,
+} from "@material-ui/core";
+import LoginButton from "./components/login-button";
+import LogoutButton from "./components/logout-button";
+import TodoList from "./components/TodoList";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const useStyles = makeStyles(theme => ({
-  main: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'stretch'
-  }
+const useStyles = makeStyles((theme) => ({
+	main: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "flex-start",
+		alignItems: "stretch",
+	},
+	title: {
+		flexGrow: 1,
+	},
 }));
 
 function App() {
+	const classes = useStyles();
+	const { isLoading, isAuthenticated, loginWithRedirect } = useAuth0();
 
-  const classes = useStyles();
-
-  return (
-    <>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6">My Todos</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container fixed>
-        <Toolbar />
-        <main className={classes.main}>
-          <TodoList />
-        </main>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<AppBar position="fixed">
+				<Toolbar>
+					<Typography variant="h6" className={classes.title}>
+						My Todos
+					</Typography>
+					{isAuthenticated ? <LogoutButton /> : <LoginButton />}
+				</Toolbar>
+			</AppBar>
+			<Container fixed>
+				<Toolbar />
+				<main className={classes.main}>
+					{isLoading ? (
+						<CircularProgress />
+					) : isAuthenticated ? (
+						<TodoList />
+					) : (
+						loginWithRedirect()
+					)}
+				</main>
+			</Container>
+		</>
+	);
 }
 
 export default App;
--- SOFTENG 750/Assignments/1/code/frontend/src/index.js
+++ SOFTENG 750/Assignments/1/code/frontend/src/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { AppContextProvider } from './AppContextProvider';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import dayjs from 'dayjs'
-import DayjsUtils from '@date-io/dayjs';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { AppContextProvider } from "./AppContextProvider";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import dayjs from "dayjs";
+import DayjsUtils from "@date-io/dayjs";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+import { BrowserRouter } from "react-router-dom";
 
-const relativeTime = require('dayjs/plugin/relativeTime');
-const localizedFormat = require('dayjs/plugin/localizedFormat');
+const relativeTime = require("dayjs/plugin/relativeTime");
+const localizedFormat = require("dayjs/plugin/localizedFormat");
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 
 ReactDOM.render(
-  <MuiPickersUtilsProvider utils={DayjsUtils}>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </MuiPickersUtilsProvider>,
-  document.getElementById('root')
+	<MuiPickersUtilsProvider utils={DayjsUtils}>
+		<BrowserRouter>
+			<AppContextProvider>
+				<Auth0ProviderWithHistory>
+					<App />
+				</Auth0ProviderWithHistory>
+			</AppContextProvider>
+		</BrowserRouter>
+	</MuiPickersUtilsProvider>,
+	document.getElementById("root")
 );
 
 // Change to "register()" to enable service workers (production only)
