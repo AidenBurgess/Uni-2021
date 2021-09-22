f32da6a5b91051ccf5253ae76209094d5c2684e9
Aiden Burgess <aiden9874@gmail.com>
2021-09-22 16:49:14 +1200

    Create Client.java

---
+++ COMPSCI 711/A5/code/client/src/main/java/app/socket/Client.java
@@ -0,0 +1,38 @@
+package app.socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Client {
+    public static int PORT = 3333;
+
+    private Client instance;
+    private Socket socket;
+    private OutputStream out;
+    private InputStream in;
+
+    public Client getClient() {
+        if (instance == null) {
+            instance = new Client();
+        }
+        return instance;
+    }
+
+    private Client() {
+        try {
+            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                socket = serverSocket.accept();
+                out = new ObjectOutputStream(socket.getOutputStream());
+                in = new ObjectInputStream(socket.getInputStream());
+            }
+
+        } catch (NumberFormatException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
